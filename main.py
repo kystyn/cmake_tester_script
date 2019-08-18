cdcadd3ef80a4de7826847d1f1f8d64f5eb7730c
= <=>
2019-08-19 02:00:48 +0300

    can run tests

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import os
 testAddr = 'git@github.com:kystyn/cmake_tester.git'
 testDir = 'test'
 
+studentAddr = 'git@github.com:kystyn/cmake_student.git'
+studentDir = 'student'
+
+buildDir = 'build'
+
+generatorCMakeParam = 'Ninja'
+generator = 'ninja'
+
 def updateRepo( repoAddress, repoDir ):
+    base = os.path.abspath(os.curdir)
     if not os.path.exists(repoDir):
-        subprocess.run(["mkdir", repoDir])
+        os.mkdir(repoDir)
         os.chdir(repoDir)
         subprocess.run(["git init"], shell = True)
         subprocess.run(["git remote add origin " + repoAddress], shell = True)
@@ -14,6 +23,46 @@ def updateRepo( repoAddress, repoDir ):
     else:
         os.chdir(repoDir)
         subprocess.run(["git pull origin master"], shell = True)
-'git@github.com:kystyn/cmake_tester.git'
+    os.chdir(base)
 
-updateRepo(testAddr, testDir)
+# root and is relative to current file
+def build( root, target ):
+    base = os.path.abspath(os.curdir)
+    os.chdir(root)
+    if not os.path.exists(target):
+        os.mkdir(target)
+    subprocess.run(["cmake -B " + target + " -G " + generatorCMakeParam], shell = True)
+    os.chdir(target)
+    subprocess.run([generator], shell = True)
+    os.chdir(base)
+
+def includeTests():
+    base = os.path.abspath(os.curdir)
+    print(base)
+    cmakefile = open(studentDir + '/CMakeLists.txt', 'a')
+    cmakefile.write('include(' + base + '/' + testDir + '/CMakeLists.txt)')
+    cmakefile.close()
+
+def clear():
+    base = os.path.abspath(os.curdir)
+    os.chdir(studentDir)
+    subprocess.run(["git stash"], shell = True)
+    os.chdir(base + '/' + testDir)
+    subprocess.run(["git stash"], shell=True)
+    os.chdir(base)
+
+def test( pathToExecutable, log ):
+    base = os.path.abspath(os.curdir)
+    os.chdir(pathToExecutable)
+    subprocess.run(["ctest -O " + log], shell = True)
+    os.chdir(base)
+
+def main():
+    updateRepo(testAddr, testDir)
+    updateRepo(studentAddr, studentDir)
+    includeTests()
+    build(studentDir, buildDir)
+    test(studentDir + '/' + buildDir, "log.txt")
+    clear()
+
+main()
